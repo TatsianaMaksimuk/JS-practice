b37ffe2df6f89c4e327a34b12dce1e7d47ef3b83
Tatsiana Maksimuk <okeanoka@gmail.com>
2022-06-01 12:56:56 -0700

    added one more solution to longest word algorithm

---
+++ longestword02.js
@@ -0,0 +1,22 @@
+function findLongestWordLength(str) {
+    let currentWordLength = 0
+    let longestWordLength = 0
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] != " ") {
+            currentWordLength += 1
+        }
+        else {
+            if (currentWordLength > longestWordLength) {
+                longestWordLength = currentWordLength
+                
+            }
+            currentWordLength = 0
+        }
+    }
+    if (currentWordLength > longestWordLength) { //because our last symbol isn't " " we have to compare one more time
+        longestWordLength = currentWordLength
+    }
+    console.log(longestWordLength)
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
