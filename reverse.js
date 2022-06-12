b31afaa1fbd25a722cb68313858dcdad6bce5b00
Tatsiana Maksimuk <okeanoka@gmail.com>
2022-06-11 23:04:59 -0700

    exercises added

---
+++ reverse.js
@@ -0,0 +1,9 @@
+let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+
+
+function arrayReverse(array){
+for(let i=array.length-1;i>=0;i--){
+console.log(array[i])
+}
+}
+arrayReverse(arr)
